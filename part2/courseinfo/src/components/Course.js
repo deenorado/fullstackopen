ba80ef418d52d92e37e1bb1d482a6a5a49d39663
Jan Deen Matulin <jdmatulin@gmail.com>
2021-07-02 05:22:19 +0800

    Added answers to activity 2.3, 2.4, 2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,29 +1,29 @@
 import React from "react";
 
 const Course = ({ course }) => {
-  let total = 0;
-
-  course.parts
-    .map((part) => part.exercises)
-    .forEach((element) => {
-      total += element;
-    });
-
   return (
     <>
-      <h1>{course.name}</h1>
-      <ul>
-        {course.parts.map((part) => {
-          return (
-            <li key={part.id}>
-              {part.name} {part.exercises}
-            </li>
-          );
-        })}
-      </ul>
-      <p>
-        <strong>total of {total} exercises</strong>
-      </p>
+      {course.map((course) => (
+        <>
+          <h1>{course.name}</h1>
+          <ul>
+            {course.parts.map((part) => {
+              return (
+                <li key={part.id}>
+                  {part.name} {part.exercises}
+                </li>
+              );
+            })}
+          </ul>
+          <p>
+            <strong>
+              total of{" "}
+              {course.parts.reduce((prev, curr) => prev + curr.exercises, 0)}{" "}
+              exercises
+            </strong>
+          </p>
+        </>
+      ))}
     </>
   );
 };
